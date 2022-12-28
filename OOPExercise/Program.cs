6e7fa450b7a81afb32e667944a738c0a9f0b556d
HamraAygun <hamraaygn@gmail.com>
2022-12-28 15:26:12 +0300

    Add project files.

---
+++ OOPExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOPExercise;
+
+
+    internal class Program
+{
+
+    //İnsan isimli bir sınıf içinde, ad, soyad, boy, kilo, yaş alanlarını tanımlayıp, bir kurucu kurucu metot içinde bu alanlar için varsayılan değer atamalarını yapacağız. Son olarak da class için "Tanis" isimli metod ile girilen bilgileri kullancıya belli, açıklayıcı bir şekilde görüntüleyen bir programcık yazınız.
+
+    // Örnek : Ben Hamra AYGÜN... Boyum 165cm, Kilom 55kg, yaşım 27'dir.
+
+    private static void Main(string[] args)
+    {
+
+
+        clsInsan clsInsan = new clsInsan("Hamra", "Aygün");
+
+        clsInsan.Tanis();
+
+        Console.ReadKey();
+
+    }
+}
---
+++ OOPExercise/clsInsan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise
+{
+    internal class clsInsan
+    {
+        public clsInsan(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+
+
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public int Boy { get; set; } = 165;
+        public int Kilo { get; set; } = 55;
+        public int Yas { get; set; } = 27;
+
+
+        public void Tanis()
+        {
+            Console.WriteLine("Ben {0} {1} ... Boyum {2} cm, Kilom {3} kg, Yaşım {4} 'dir", Name, Surname, Boy, Kilo, Yas);
+
+        }
+
+    }
+}
